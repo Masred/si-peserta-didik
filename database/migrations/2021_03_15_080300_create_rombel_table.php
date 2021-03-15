d821c222eb12769a2d051cef4eb66879187ad42e
Dwi Fitriyanto <dwifitriyanto123@gmail.com>
2021-03-15 19:29:19 +0700

    add table rombel

---
+++ database/migrations/2021_03_15_080300_create_rombel_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRombelTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rombel', function (Blueprint $table) {
+            $table->integer('id', true);
+            $table->integer('jurusan_id');
+            $table->string('kelas', 3);
+            $table->smallInteger('kelompok');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rombel');
+    }
+}
