721d830acad67510fdfbced3bc0ba4f473ce1191
Dwi Fitriyanto <dwifitriyanto123@gmail.com>
2021-03-23 16:41:59 +0700

    create JurusanController

---
+++ app/Http/Controllers/JurusanController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\JurusanModel;
+use Illuminate\Http\Request;
+
+class JurusanController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\JurusanModel  $jurusanModel
+     * @return \Illuminate\Http\Response
+     */
+    public function show(JurusanModel $jurusanModel)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\JurusanModel  $jurusanModel
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(JurusanModel $jurusanModel)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\JurusanModel  $jurusanModel
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, JurusanModel $jurusanModel)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\JurusanModel  $jurusanModel
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(JurusanModel $jurusanModel)
+    {
+        //
+    }
+}
