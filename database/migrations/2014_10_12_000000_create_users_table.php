1f824e7dd0abb5482a3aebe3cc225cbf803fae92
Dwi Fitriyanto <dwifitriyanto123@gmail.com>
2021-03-23 15:15:38 +0700

    change name to nama and level to role

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,10 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('nama');
             $table->string('username')->unique();
             $table->string('password');
-            $table->enum('level', ['admin', 'biasa']);
+            $table->enum('role', ['admin', 'user']);
         });
     }
 
