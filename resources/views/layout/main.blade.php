a5d60e797789b881d3921ce58d039cd8f5d02d98
Dwi Fitriyanto <dwifitriyanto123@gmail.com>
2021-03-25 09:48:48 +0700

    add script yield

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -47,7 +47,8 @@
             <!-- Sidebar user panel (optional) -->
             <div class="user-panel mt-3 pb-3 mb-3 d-flex">
                 <div class="image img-size-64">
-                    <img src="{{ asset('img/avatar/administrator.png') }}" class="img-circle elevation-2" alt="User Image">
+                    <img src="{{ asset('img/avatar/administrator.png') }}" class="img-circle elevation-2"
+                         alt="User Image">
                 </div>
                 <div class="info">
                     <a href="#" class="d-block">Alexander Pierce</a>
@@ -163,5 +164,7 @@
 <script src="{{ asset('plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
 <!-- AdminLTE App -->
 <script src="{{ asset('dist/js/adminlte.min.js') }}"></script>
+<!-- additional script -->
+@yield('script')
 </body>
 </html>
