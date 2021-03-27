42ac72a7ab7b7a86e4e6746e032267e1551f0da7
Dwi Fitriyanto <dwifitriyanto123@gmail.com>
2021-03-27 20:07:25 +0700

    append kelompok

--- database/factories/RombelFactory.php
+++ database/factories/RombelFactory.php
@@ -22,10 +22,10 @@ class RombelFactory extends Factory
     public function definition()
     {
         return [
-            'nama_rombel' => $this->faker->randomElement(array('X', 'XI', 'XII')) . ' ' . $this->faker->randomElement(array('DKV', 'KLT', 'KYU', 'LGM', 'RPL', 'TKL', 'TKR')) . ' ' . $this->faker->randomElement(array(1, 2, 3)),
+            'nama_rombel' => $this->faker->randomElement(array('X', 'XI', 'XII')) . ' ' . $this->faker->randomElement(array('DKV', 'KLT', 'KYU', 'LGM', 'RPL', 'TKL', 'TKR')) . ' ' . $this->faker->randomElement(array(1, 2, 3, 4, 5)),
             'kelas' => $this->faker->randomElement(array('X', 'XI', 'XII')),
             'kode_jurusan' => $this->faker->randomElement(array('DKV', 'KLT', 'KYU', 'LGM', 'RPL', 'TKL', 'TKR')),
-            'kelompok' => $this->faker->randomElement(array(1, 2, 3))
+            'kelompok' => $this->faker->randomElement(array(1, 2, 3, 4, 5))
         ];
     }
 }
