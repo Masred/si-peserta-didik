3d107271adbd84f1dedf6fab5c5b37e2f0aec63d
Dwi Fitriyanto <dwifitriyanto123@gmail.com>
2021-03-27 19:50:55 +0700

    add RombelFactory

---
+++ database/factories/RombelFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Rombel;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class RombelFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Rombel::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'nama_rombel' => $this->faker->randomElement(array('X', 'XI', 'XII')) . ' ' . $this->faker->randomElement(array('DKV', 'KLT', 'KYU', 'LGM', 'RPL', 'TKL', 'TKR')) . ' ' . $this->faker->randomElement(array(1, 2, 3)),
+            'kelas' => $this->faker->randomElement(array('X', 'XI', 'XII')),
+            'kode_jurusan' => $this->faker->randomElement(array('DKV', 'KLT', 'KYU', 'LGM', 'RPL', 'TKL', 'TKR')),
+            'kelompok' => $this->faker->randomElement(array(1, 2, 3))
+        ];
+    }
+}
