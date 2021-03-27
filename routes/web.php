c5b7e59fab10233da295f884832694c911ce9c25
Dwi Fitriyanto <dwifitriyanto123@gmail.com>
2021-03-27 15:05:36 +0700

    add routes for CRUD Rombel

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\JurusanController;
+use App\Http\Controllers\RombelController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -18,3 +19,4 @@ Route::get('/', function () {
 });
 
 Route::resource('jurusan', JurusanController::class);
+Route::resource('rombel', RombelController::class);
