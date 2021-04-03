2a1f9604f8e27653dd95a80944c619f9681ee002
Dwi Fitriyanto <dwifitriyanto123@gmail.com>
2021-04-03 21:35:59 +0700

    delete some field, add tahun masuk and tahun keluar

--- app/Models/PesertaDidik.php
+++ app/Models/PesertaDidik.php
@@ -11,9 +11,10 @@ class PesertaDidik extends Model
 
     public $timestamps = false;
     protected $table = 'peserta_didik';
-    protected $fillable = ['status','nama','nipd','jenis_kelamin','nisn','tempat_lahir','tanggal_lahir','nik','no_kk','agama','alamat','kelurahan','kecamatan','kota','tempat_tinggal','moda_transportasi','hp','email','anak_ke','jumlah_saudara_kandung','jarak_rumah_ke_sekolah','nama_ayah','tahun_lahir_ayah','jenjang_pendidikan_ayah','pekerjaan_ayah','penghasilan_ayah','nik_ayah','nama_ibu','tahun_lahir_ibu','jenjang_pendidikan_ibu','pekerjaan_ibu','penghasilan_ibu','nik_ibu','nama_wali','tahun_lahir_wali','jenjang_pendidikan_wali','pekerjaan_wali','penghasilan_wali','nik_wali', 'rombel_id'];
+    protected $fillable = ['status', 'nama', 'nipd', 'jenis_kelamin', 'nisn', 'tempat_lahir', 'tahun_masuk', 'tahun_keluar', 'tanggal_lahir', 'nik', 'no_kk', 'agama', 'alamat', 'kelurahan', 'kecamatan', 'kota', 'hp', 'email', 'nama_ayah', 'tahun_lahir_ayah', 'jenjang_pendidikan_ayah', 'pekerjaan_ayah', 'penghasilan_ayah', 'nik_ayah', 'nama_ibu', 'tahun_lahir_ibu', 'jenjang_pendidikan_ibu', 'pekerjaan_ibu', 'penghasilan_ibu', 'nik_ibu', 'nama_wali', 'tahun_lahir_wali', 'jenjang_pendidikan_wali', 'pekerjaan_wali', 'penghasilan_wali', 'nik_wali', 'rombel_id'];
 
-    public function rombel(){
+    public function rombel()
+    {
         return $this->belongsTo(Rombel::class);
     }
 }
