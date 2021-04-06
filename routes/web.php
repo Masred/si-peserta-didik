12947ec0550e42755ae1c8a50a68d6d375d7df96
Dwi Fitriyanto <dwifitriyanto123@gmail.com>
2021-04-06 20:13:06 +0700

    add route for export and print

--- routes/web.php
+++ routes/web.php
@@ -23,12 +23,14 @@ Route::get('/', function () {
 Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard.index');
 
 Route::delete('jurusan/multiple-destroy', [JurusanController::class, 'multiple_destroy'])->name('jurusan.multiple-destroy');
-Route::delete('rombel/multiple-destroy', [RombelController::class, 'multiple_destroy'])->name('rombel.multiple-destroy');
-Route::delete('peserta-didik/multiple-destroy', [PesertaDidikController::class, 'multiple_destroy'])->name('peserta-didik.multiple-destroy');
-
-Route::post('peserta-didik/import', [PesertaDidikController::class, 'import'])->name('peserta-didik.import');
-Route::get('peserta-didik/export', [PesertaDidikController::class, 'export'])->name('peserta-didik.export');
-
 Route::resource('jurusan', JurusanController::class);
+
+Route::delete('rombel/multiple-destroy', [RombelController::class, 'multiple_destroy'])->name('rombel.multiple-destroy');
 Route::resource('rombel', RombelController::class);
+
+Route::post('peserta-didik/print', [PesertaDidikController::class, 'print'])->name('peserta-didik.print');
+Route::delete('peserta-didik/multiple-destroy', [PesertaDidikController::class, 'multiple_destroy'])->name('peserta-didik.multiple-destroy');
+Route::post('peserta-didik/import', [PesertaDidikController::class, 'import'])->name('peserta-didik.import');
+Route::get('peserta-didik/export-excel', [PesertaDidikController::class, 'excel'])->name('peserta-didik.export-excel');
+Route::get('peserta-didik/export-pdf', [PesertaDidikController::class, 'pdf'])->name('peserta-didik.export-pdf');
 Route::resource('peserta-didik', PesertaDidikController::class);
