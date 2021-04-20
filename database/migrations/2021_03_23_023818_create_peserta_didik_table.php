a15bf2adf0f2a87ee1bb9864391207f9a2ac1cff
Dwi Fitriyanto <dwifitriyanto123@gmail.com>
2021-04-20 11:24:18 +0700

    alter table

--- database/migrations/2021_03_23_023818_create_peserta_didik_table.php
+++ database/migrations/2021_03_23_023818_create_peserta_didik_table.php
@@ -15,24 +15,29 @@ class CreatePesertaDidikTable extends Migration
     {
         Schema::create('peserta_didik', function (Blueprint $table) {
             $table->id();
+            $table->enum('status', ['aktif', 'keluar']);
+            $table->enum('terdaftar_sebagai', ['Siswa baru', 'Pindahan', 'Kembali bersekolah']);
+            $table->string('pindahan_dari')->nullable();
             $table->string('nama')->nullable();
-            $table->enum('status', ['aktif', 'pindah', 'tamat', 'pindahan', 'keluar', 'dikeluarkan']);
-            $table->enum('jenis_kelamin',['L','P'])->nullable();
             $table->string('nipd')->nullable();
+            $table->enum('jenis_kelamin', ['L', 'P'])->nullable();
             $table->string('nisn')->nullable();
-            $table->year('tahun_masuk')->nullable();
             $table->string('tempat_lahir')->nullable();
             $table->date('tanggal_lahir')->nullable();
             $table->bigInteger('nik')->nullable();
-            $table->bigInteger('no_kk')->nullable();
             $table->string('agama')->nullable();
+            $table->date('tanggal_masuk')->nullable();
+            $table->enum('keluar_karena', ['Lulus', 'Mutasi', 'Dikeluarkan', 'Mengundurkan diri', 'Putus Sekolah', 'Wafat', 'Hilang'])->default(null)->nullable();
+            $table->string('alasan_keluar')->nullable();
+            $table->date('tanggal_keluar')->nullable();
             $table->text('alamat')->nullable();
+            $table->tinyInteger('rt')->nullable();
+            $table->tinyInteger('rw')->nullable();
             $table->string('kelurahan')->nullable();
             $table->string('kecamatan')->nullable();
-            $table->string('kota')->nullable();
+            $table->smallInteger('kode_pos')->nullable();
             $table->string('hp', 20)->nullable();
             $table->string('email', 50)->nullable();
-            $table->year('tahun_keluar')->nullable();
             $table->string('nama_ayah')->nullable();
             $table->year('tahun_lahir_ayah')->nullable();
             $table->string('jenjang_pendidikan_ayah')->nullable();
@@ -51,6 +56,8 @@ class CreatePesertaDidikTable extends Migration
             $table->string('pekerjaan_wali')->nullable();
             $table->string('penghasilan_wali')->nullable();
             $table->bigInteger('nik_wali')->nullable();
+            $table->string('sekolah_asal')->nullable();
+            $table->bigInteger('no_kk')->nullable();
             $table->string('kode_rombel', 15)->nullable();
             $table->foreign('kode_rombel')->references('kode_rombel')->on('rombel')->nullOnDelete()->cascadeOnUpdate();
         });
