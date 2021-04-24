4cbb72de1ed304a8619a9c3694dc0ab783f50211
Dwi Fitriyanto <dwifitriyanto123@gmail.com>
2021-04-24 14:26:12 +0700

    add rombel data

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Jurusan;
 use App\Models\PesertaDidik;
 use App\Models\Rombel;
-use Illuminate\Http\Request;
+use App\Models\Surat;
 
 class DashboardController extends Controller
 {
@@ -14,6 +14,24 @@ class DashboardController extends Controller
         $total_jurusan = Jurusan::all()->count();
         $total_rombel = Rombel::all()->count();
         $total_peserta_didik = PesertaDidik::all()->count();
-        return view('dashboard.index', compact('total_jurusan', 'total_peserta_didik', 'total_rombel'));
+        $total_surat = Surat::all()->count();
+
+        $rombels = Rombel::withCount([
+            'pesertaDidik',
+            'pesertaDidik as lakilaki' => function ($query) {
+                $query->where('jenis_kelamin', '=', 'L');
+            },
+            'pesertaDidik as perempuan' => function ($query) {
+                $query->where('jenis_kelamin', '=', 'P');
+            },
+            'pesertaDidik as lulus' => function ($query) {
+                $query->where('keluar_karena', '=', 'Lulus');
+            },
+            'pesertaDidik as mutasi' => function ($query) {
+                $query->where('keluar_karena', '=', 'Mutasi');
+            }
+        ])->get();
+
+        return view('dashboard.index', compact('total_jurusan', 'total_peserta_didik', 'total_rombel', 'total_surat', 'rombels'));
     }
 }
