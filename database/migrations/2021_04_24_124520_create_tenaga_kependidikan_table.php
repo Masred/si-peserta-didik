741253947bd23f06dcfcb358346eb67595669442
Dwi Fitriyanto <dwifitriyanto123@gmail.com>
2021-04-25 15:22:44 +0700

    create Tenaga Kependidikan Table

---
+++ database/migrations/2021_04_24_124520_create_tenaga_kependidikan_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTenagaKependidikanTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tenaga_kependidikan', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama')->nullable();
+            $table->bigInteger('nik')->nullable();
+            $table->bigInteger('nip')->nullable();
+            $table->enum('jenis_kelamin', ['L', 'P'])->nullable();
+            $table->string('tempat_lahir')->nullable();
+            $table->date('tanggal_lahir')->nullable();
+            $table->string('agama')->nullable();
+            $table->string('alamat')->nullable();
+            $table->string('rt')->nullable();
+            $table->string('rw')->nullable();
+            $table->string('kelurahan')->nullable();
+            $table->string('kecamatan')->nullable();
+            $table->integer('kode_pos')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tenaga_kependidikan');
+    }
+}
