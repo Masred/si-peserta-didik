2ca09be762bdf128f0dfbfe244d3725a43db4b35
Dwi Fitriyanto <dwifitriyanto123@gmail.com>
2021-04-26 12:00:47 +0700

    adjust ui

--- resources/views/peserta-didik/print.blade.php
+++ resources/views/peserta-didik/print.blade.php
@@ -1,7 +1,23 @@
 @extends('layouts.kop-surat')
 @section('title', 'Data Peserta Didik')
 @section('content')
-    <h3 align="center" style="margin: 10px">DATA PESERTA DIDIK KELAS {{ ($kelas === 'Semua')? '10-12': $kelas }}</h3>
+    <h4 align="center" style="margin: 10px">
+        @if($keterangan == 'AKTIF')
+            DATA PESERTA DIDIK AKTIF
+        @elseif($keterangan == 'ALUMNI')
+            DATA ALUMNI
+        @elseif($keterangan == 'ALUMNI-JURUSAN')
+            DATA ALUMNI JURUSAN {{ strtoupper($peserta_didik[0]->nama_jurusan) }}
+        @elseif($keterangan == 'KELAS')
+            DATA PESERTA DIDIK AKTIF KELAS @if($peserta_didik[0]->kelas == 'X')
+                10 @elseif($peserta_didik[0]->kelas == 'XI') 11 @elseif($peserta_didik[0]->kelas == 'XII')
+                12 @else {{ $peserta_didik[0]->kode_rombel }} @endif
+        @elseif($keterangan == 'JURUSAN')
+            DATA PESERTA DIDIK AKTIF KELAS @if($peserta_didik[0]->kelas == 'X')
+                10 @elseif($peserta_didik[0]->kelas == 'XI') 11 @elseif($peserta_didik[0]->kelas == 'XII') 12 @endif
+            JURUSAN {{ strtoupper($peserta_didik[0]->nama_jurusan) }}
+        @endif
+    </h4>
     <table width="100%" border="1px" cellpadding="5px" cellspacing="0"
            style="font-size: 10px;font-family: Arial,serif;">
         <tr>
