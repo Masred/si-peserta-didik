fb2cebccf7e76d54c7de256c14dfa4f13467ffda
Dwi Fitriyanto <dwifitriyanto123@gmail.com>
2021-04-26 07:26:13 +0700

    remove unused namespace

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,9 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Validator;
 use Illuminate\Validation\Rule;
-use Laravel\Fortify\Rules\Password;
 
 class UserController extends Controller
 {
