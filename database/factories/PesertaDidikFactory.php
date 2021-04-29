3e4e818739b07a2361b2446dfa1d6755e56eec50
Dwi Fitriyanto <dwifitriyanto123@gmail.com>
2021-04-29 07:02:51 +0700

    edit peserta didik factory

--- database/factories/PesertaDidikFactory.php
+++ database/factories/PesertaDidikFactory.php
@@ -24,18 +24,14 @@ class PesertaDidikFactory extends Factory
     {
         $this->faker = \Faker\Factory::create('id_ID');
         return [
-            'status' => $this->faker->randomElement(array('aktif', 'keluar')),
+            'status' => $this->faker->randomElement(array('aktif')),
             'jenis_pendaftaran' => $this->faker->randomElement(array('Siswa baru', 'Pindahan', 'Kembali bersekolah')),
-            'nama' => $this->faker->firstName,
+            'nama' => $this->faker->firstName . ' ' . $this->faker->lastName,
             'jenis_kelamin' => $this->faker->randomElement(array('L', 'P')),
             'nipd' => $this->faker->numberBetween(26773, 934512),
             'nisn' => $this->faker->numberBetween(6773, 97732),
-            'tanggal_masuk' => $this->faker->date('Y-m-d'),
-            'keluar_karena' => $this->faker->randomElement(array('Lulus', 'Mutasi', 'Dikeluarkan', 'Mengundurkan diri', 'Putus Sekolah', 'Wafat', 'Hilang')),
-            'alasan_keluar' => $this->faker->randomElement(array('keinginan orang tua', 'jauh dari rumah', 'mengikuti orang tua')),
-            'tanggal_keluar' => $this->faker->date('Y-m-d'),
             'tempat_lahir' => $this->faker->city,
-            'tanggal_lahir' => $this->faker->date(),
+            'tanggal_lahir' => $this->faker->date('Y-m-d', '-15 years'),
             'nik' => $this->faker->numberBetween(3274839283748574, 3299999999999999),
             'no_kk' => $this->faker->numberBetween(3274839283748574, 3299999999999999),
             'agama' => 'Islam',
@@ -66,7 +62,7 @@ class PesertaDidikFactory extends Factory
             'penghasilan_wali' => $this->faker->randomElement(array('Rp. 500.000 - 999.9999', 'Rp. 2 juta - 4.999.999 ', 'Rp. 5 juta - 20 juta')),
             'nik_wali' => $this->faker->numberBetween(3274839283748574, 3299999999999999),
             'sekolah_asal' => 'SMP Negeri ' . $this->faker->randomDigit . ' ' . $this->faker->city,
-            'kode_rombel' => $this->faker->randomElement(array('X-DKV-1', 'X-DKV-2','X-DKV-3', 'X-RPL-1', 'X-RPL-2', 'X-RPL-3', 'XI-DKV-1', 'XI-DKV-2', 'XI-DKV-3', 'XI-RPL-1', 'XI-RPL-2', 'XI-RPL-3', 'XII-DKV-1', 'XII-DKV-2', 'XII-DKV-3', 'XII-RPL-1', 'XII-RPL-2', 'XII-RPL-3'))
+            'kode_rombel' => $this->faker->randomElement(array('X-DKV-1', 'X-DKV-2','X-DKV-3', 'X-KULIT-1', 'X-KULIT-2', 'X-KULIT-3', 'XI-DKV-1', 'XI-DKV-2', 'XI-DKV-3', 'XI-KULIT-1', 'XI-KULIT-2', 'XI-KULIT-3', 'XII-DKV-1', 'XII-DKV-2', 'XII-DKV-3', 'XII-KULIT-1', 'XII-KULIT-2', 'XII-KULIT-3'))
         ];
     }
 }
