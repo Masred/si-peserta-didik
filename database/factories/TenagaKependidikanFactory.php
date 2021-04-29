2d9bcfc84ba01b5c45391c0de12f17e838537bdd
Dwi Fitriyanto <dwifitriyanto123@gmail.com>
2021-04-29 07:03:51 +0700

    add tendik factory

---
+++ database/factories/TenagaKependidikanFactory.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\TenagaKependidikan;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class TenagaKependidikanFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = TenagaKependidikan::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'nama' => $this->faker->name,
+            'nik' => $this->faker->numberBetween('3286986879', '329345345346'),
+            'nip' => $this->faker->numberBetween('86986879', '9345345346'),
+            'keterangan' => 'Pegawai',
+            'jenis_kelamin' => $this->faker->randomElement(array('L', 'P')),
+            'tempat_lahir' => $this->faker->city,
+            'tanggal_lahir' => $this->faker->date('Y-m-d', '-20 years'),
+            'agama' => 'Islam',
+            'alamat' => $this->faker->streetAddress,
+            'rt' => '01',
+            'rw' => '12',
+            'kelurahan' => 'tamanjaya',
+            'kecamatan' => 'tamansasri',
+            'kode_pos' => 47683,
+        ];
+    }
+}
