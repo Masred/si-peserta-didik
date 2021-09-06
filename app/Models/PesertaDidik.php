a4d8dd89d35f7714cc189ef3bf6bd79d8c54c0fe
Dwi Fitriyanto <dwifitriyanto123@gmail.com>
2021-09-06 12:02:00 +0700

    remove relation with rombel

--- app/Models/PesertaDidik.php
+++ app/Models/PesertaDidik.php
@@ -10,16 +10,13 @@ class PesertaDidik extends Model
     use HasFactory;
 
     public $timestamps = false;
+    public $incrementing = false;
     protected $table = 'peserta_didik';
-    protected $fillable = ['status', 'jenis_pendaftaran', 'sekolah_asal', 'nama', 'nipd', 'jenis_kelamin', 'nisn', 'tempat_lahir', 'tanggal_masuk', 'keluar_karena', 'pindah_ke', 'alasan_keluar', 'tanggal_keluar', 'tanggal_lahir', 'nik', 'no_kk', 'agama', 'alamat', 'rt', 'rw', 'kelurahan', 'kecamatan', 'kode_pos', 'hp', 'email', 'nama_ayah', 'tahun_lahir_ayah', 'jenjang_pendidikan_ayah', 'pekerjaan_ayah', 'penghasilan_ayah', 'nik_ayah', 'nama_ibu', 'tahun_lahir_ibu', 'jenjang_pendidikan_ibu', 'pekerjaan_ibu', 'penghasilan_ibu', 'nik_ibu', 'nama_wali', 'tahun_lahir_wali', 'jenjang_pendidikan_wali', 'pekerjaan_wali', 'penghasilan_wali', 'nik_wali', 'kode_rombel'];
-
-    public function rombel()
-    {
-        return $this->belongsTo(Rombel::class, 'kode_rombel');
-    }
+    protected $primaryKey = 'nipd';
+    protected $fillable = ['status', 'jenis_pendaftaran', 'sekolah_asal', 'nama', 'nipd', 'jenis_kelamin', 'nisn', 'tempat_lahir', 'tanggal_masuk', 'keluar_karena', 'pindah_ke', 'alasan_keluar', 'tanggal_keluar', 'tanggal_lahir', 'nik', 'no_kk', 'agama', 'alamat', 'rt', 'rw', 'kelurahan', 'kecamatan', 'kode_pos', 'hp', 'email', 'nama_ayah', 'tahun_lahir_ayah', 'jenjang_pendidikan_ayah', 'pekerjaan_ayah', 'penghasilan_ayah', 'nik_ayah', 'nama_ibu', 'tahun_lahir_ibu', 'jenjang_pendidikan_ibu', 'pekerjaan_ibu', 'penghasilan_ibu', 'nik_ibu', 'nama_wali', 'tahun_lahir_wali', 'jenjang_pendidikan_wali', 'pekerjaan_wali', 'penghasilan_wali', 'nik_wali'];
 
     public function surat()
     {
-        return $this->hasMany(Surat::class);
+        return $this->hasMany(Surat::class, 'nipd');
     }
 }
