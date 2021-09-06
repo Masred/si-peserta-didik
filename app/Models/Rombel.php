0e2b240529222b3c1dffecef5e86292f4f85f741
Dwi Fitriyanto <dwifitriyanto123@gmail.com>
2021-09-06 12:01:43 +0700

    remove relation with peserta_didik table

--- app/Models/Rombel.php
+++ app/Models/Rombel.php
@@ -9,20 +9,17 @@ class Rombel extends Model
 {
     use HasFactory;
     public $timestamps = false;
-    public $incrementing = false;
     protected $table = 'rombel';
     protected $primaryKey = 'kode_rombel';
-    protected $fillable = ['kode_rombel', 'kelas', 'kode_jurusan', 'kelompok', 'guru_id'];
+    protected $fillable = ['kelas', 'kode_jurusan', 'kelompok', 'guru_id'];
 
-    public function jurusan(){
+    public function jurusan()
+    {
         return $this->belongsTo(Jurusan::class, 'kode_jurusan');
     }
 
-    public function pesertaDidik(){
-        return $this->hasMany(PesertaDidik::class, 'kode_rombel');
-    }
-
-    public function guru(){
+    public function guru()
+    {
         return $this->belongsTo(Guru::class);
     }
 }
